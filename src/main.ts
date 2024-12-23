78464e3efb7e4920420e2fbb342004bee37a6dfa
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-23 15:51:44 +0200

    hook husky

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 
-import { readFile, writeFile } from "./services/fs.service";
+import { readFile } from "./services/fs.service";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-
 console.log(process.env.PORT);
 
 const func = async () => {
@@ -37,98 +36,94 @@ const func = async () => {
       }
     },
   );
-
-  app.post("/users", async (req: Request, res: Response): Promise<void> => {
-    try {
-      const users = await readFile();
-      const { name, age, email, isActive } = req.body;
-
-      if (
-        typeof name !== "string" ||
-                   typeof age !== "number" ||
-        typeof email !== "string" ||
-        typeof isActive !== "boolean"
-      ) {
-        return res.sendStatus(400);
-      }
-
-      const user = {
-        id: users.length ? users[users.length - 1].id + 1 : 1,
-        name,
-        age,
-        email,
-        isActive,
-      };
-
-      users.push(user);
-      await writeFile(users);
-      return res.sendStatus(204);
-    } catch (e) {
-      return res.status(500).json({ error: e.message });
-    }
-  });
-  app.delete(
-    "/users/:userId",
-    async (req: Request, res: Response): Promise<void> => {
-      try {
-        const users = await readFile();
-        const pmUserId = +req.params.userId;
-        const foundUserIndex = users.findIndex((user) => user.id === pmUserId);
-
-        if (foundUserIndex === -1) {
-          return res.sendStatus(404);
-        }
-
-        users.splice(foundUserIndex, 1);
-        await writeFile(users);
-        return res.sendStatus(204);
-      } catch (e) {
-        return res.status(500).json({ error: e.message });
-      }
-    },
-  );
-
-  app.put(
-    "/users/:userId",
-    async (req: Request, res: Response): Promise<void> => {
-      try {
-        const users = await readFile();
-        const { name, age, email, isActive } = req.body;
-        const pmUserId = +req.params.userId;
-
-        const foundUserIndex = users.findIndex((user) => user.id === pmUserId);
-        if (foundUserIndex === -1) {
-          return res.sendStatus(404);
-        }
-
-        if (
-          typeof name !== "string" ||
-          typeof age !== "number" ||
-          typeof email !== "string" ||
-          typeof isActive !== "boolean"
-        ) {
-          return res.sendStatus(400);
-        }
-
-        users[foundUserIndex] = {
-          id: pmUserId,
-          name,
-          age,
-          email,
-          isActive,
-        };
-
-        await writeFile(users);
-        return res.sendStatus(204);
-      } catch (e) {
-        return res.status(500).json({ error: e.message });
-      }
-    },
-  );
-
+  //
+  // app.post("/users", async (req: Request, res: Response): Promise<void> => {
+  //   try {
+  //     const users = await readFile();
+  //     const { name, age, email, isActive } = req.body;
+  //
+  //     if (
+  //       typeof name !== "string" ||
+  //       typeof age !== "number" ||
+  //       typeof email !== "string" ||
+  //       typeof isActive !== "boolean"
+  //     ) {
+  //       return res.sendStatus(400);
+  //     }
+  //
+  //     const user = {
+  //       id: users.length ? users[users.length - 1].id + 1 : 1,
+  //       name,
+  //       age,
+  //       email,
+  //       isActive,
+  //     };
+  //
+  //     users.push(user);
+  //     await writeFile(users);
+  //     return res.sendStatus(204);
+  //   } catch (e) {
+  //     return res.status(500).json({ error: e.message });
+  //   }
+  // });
+  // app.delete(
+  //   "/users/:userId",
+  //   async (req: Request, res: Response): Promise<void> => {
+  //     try {
+  //       const users = await readFile();
+  //       const pmUserId = +req.params.userId;
+  //       const foundUserIndex = users.findIndex((user) => user.id === pmUserId);
+  //
+  //       if (foundUserIndex === -1) {
+  //         return res.sendStatus(404);
+  //       }
+  //
+  //       users.splice(foundUserIndex, 1);
+  //       await writeFile(users);
+  //       return res.sendStatus(204);
+  //     } catch (e) {
+  //       return res.status(500).json({ error: e.message });
+  //     }
+  //   },
+  // );
+  //
+  // app.put(
+  //   "/users/:userId",
+  //   async (req: Request, res: Response): Promise<void> => {
+  //     try {
+  //       const users = await readFile();
+  //       const { name, age, email, isActive } = req.body;
+  //       const pmUserId = +req.params.userId;
+  //
+  //       const foundUserIndex = users.findIndex((user) => user.id === pmUserId);
+  //       if (foundUserIndex === -1) {
+  //         return res.sendStatus(404);
+  //       }
+  //
+  //       if (
+  //         typeof name !== "string" ||
+  //         typeof age !== "number" ||
+  //         typeof email !== "string" ||
+  //         typeof isActive !== "boolean"
+  //       ) {
+  //         return res.sendStatus(400);
+  //       }
+  //
+  //       users[foundUserIndex] = {
+  //         id: pmUserId,
+  //         name,
+  //         age,
+  //         email,
+  //         isActive,
+  //       };
+  //
+  //       await writeFile(users);
+  //       return res.sendStatus(204);
+  //     } catch (e) {
+  //       return res.status(500).json({ error: e.message });
+  //     }
   app.listen(process.env.PORT, () => {
     console.log(`http://localhost:${process.env.PORT}/users`);
   });
 };
-
 void func();
