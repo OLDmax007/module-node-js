2c536ccc0f7550d03717b4e866e9d54878a306c5
OLDmax007 <maxsim.dobrovolskyimd@gmail.com>
2024-12-29 19:47:02 +0200

    hw 5 and validation with middlewares

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,12 +1,20 @@
 import { model, Schema } from "mongoose";
 
+import { RoleEnum } from "../enums/role.enum";
+
 const userSchema = new Schema(
   {
     name: { type: String, required: true, trim: true },
     email: { type: String, required: true, unique: true, trim: true },
     age: { type: Number, required: true, trim: true },
     password: { type: String, required: true, trim: true },
-    role: { type: String, required: false, trim: true, default: "user" },
+    role: {
+      enum: RoleEnum,
+      type: String,
+      required: false,
+      trim: true,
+      default: RoleEnum.USER,
+    },
     phone: { type: String, required: false, trim: true },
     isDeleted: { type: Boolean, required: false, default: false },
     isVerifed: { type: Boolean, required: false, default: false },
--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,50 +1,21 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
-import ApiError from "../errors/api-error";
-
-const validateUserData = (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const dto = req.body;
-
-    if (
-      typeof dto.name !== "string" ||
-      typeof dto.age !== "number" ||
-      typeof dto.email !== "string"
-    ) {
-      throw new ApiError("Incorrect entered data", 400);
-    }
-
-    if (!dto.name || dto.name.length < 3) {
-      throw new ApiError(
-        "Name is required and should be minimum 3 symbols",
-        400
-      );
-    }
-
-    if (!dto.email || !dto.email.includes("@")) {
-      throw new ApiError("Email is required", 400);
-    }
-
-    if (!dto.password || dto.password.length < 8) {
-      throw new ApiError(
-        "Password is required and should be minimum 8 symbols",
-        400
-      );
-    }
-
-    req.body = dto;
-    next();
-  } catch (e) {
-    next(e);
-  }
-};
+import { commonMiddleware } from "../middlewares/common.middleware";
 
 const router = Router();
 router.get("/", userController.getItems);
-router.get("/:userId", userController.getById);
-router.post("/", validateUserData, userController.create);
-router.put("/:userId", validateUserData, userController.update);
-router.delete("/:userId", userController.delete);
+router.get("/:userId", commonMiddleware.isIdValid("userId"));
+router.post("/", commonMiddleware.validateUserData, userController.create);
+router.put(
+  "/:userId",
+  commonMiddleware.validateUserData,
+  userController.update
+);
+router.delete(
+  "/:userId",
+  commonMiddleware.isIdValid("userId"),
+  userController.delete
+);
 
 export const userRouter = router;
